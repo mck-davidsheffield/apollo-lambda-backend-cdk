2e8fa0e71f710a724c49d78796c4f221b79f2fe5
David Sheffield <david_sheffield@mckinsey.com>
2022-09-01 07:59:57 -0400

    initial

---
+++ deployments/deploy-to-dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# temp account and credentials
+export AWS_ACCOUNT=TBD
+export AWS_REGION=us-east-1
+export AWS_ACCESS_KEY_ID="TBD"
+export AWS_SECRET_ACCESS_KEY="TBD"
+
+# vars
+export ENV_NAME=dev
+
+
+cdk bootstrap aws://$AWS_ACCOUNT/$AWS_REGION
+
+npm run build
+
+cdk deploy apollo-lambda-backend-cdk-$ENV_NAME \
+ --context envName=$ENV_NAME
+
--- lib/apollo-lambda-cdk-stack.ts
+++ lib/apollo-lambda-cdk-stack.ts
@@ -1,16 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_lambda, aws_lambda_nodejs, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as path from "path";
 
 export class ApolloLambdaCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const envName = process.env.ENV_NAME || 'dev'
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApolloLambdaCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Apollo Main Lambda Resolver
+    const apolloMainLambda = new aws_lambda_nodejs.NodejsFunction(
+      this,
+      `userAccountLambdaResolver`,
+      {
+        description: 'Lambda resolves UserAccount mutations and queries for AppSync',
+        entry: path.join(__dirname, './node-lambdas/apollo-handler.lambda.ts'),
+        handler: 'handler',
+        runtime: aws_lambda.Runtime.NODEJS_16_X,
+        timeout: Duration.seconds(55),
+        memorySize: 256,
+        environment: {
+          ENV_NAME: envName,
+          // TABLE_NAME: singleDynamoTable.tableName,
+        },
+        functionName: `apollo-main-lambda-${envName}`,
+        architecture: aws_lambda.Architecture.ARM_64,
+      }
+    );
   }
 }
